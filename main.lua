5de69eb01d1f7968bc2b414a269bf096d80fbb32
Sergey Mironov <ka6ah505@gmail.com>
2018-07-17 15:51:43 +0300

    Добавлено оповещение о средних телах последних 8-ми свечей и размере последней свечи

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 isRun = true;
-delay = 10000;
+delay = 2000;
 ds = nil;
 lastCandlePosition = 0; -- порядковый номер последней свечи
 
@@ -8,17 +8,25 @@ function main()
 	ConnectToData();
 	while isRun do
 		if mainCalculate() then
+			body = math.abs(ds:C(lastCandlePosition-1) - ds:O(lastCandlePosition-1));
+			-- open, close = ds:O(lastCandlePosition-1), ds:C(lastCandlePosition-1);
+			body = body - body%0.01;
+			avr = avrBodyCandle(8);
+			avr = avr - avr%0.01;
+			message("candle body:\t"..tostring(body).."\navr:\t\t"..tostring(avr));
+			-- message("open:\t"..tostring(open).."close:\t"..tostring(close).."\navr:\t"..tostring(avr));
 		else
+
 		end;
 		sleep(delay);
-	end
+	end;
 end;
 
 -- Функция подключения к данным инструмента
 function ConnectToData()
 	-- body
 	Error = nil
-	ds, Error = CreateDataSource("SPBFUT", "BRQ8", INTERVAL_M2);
+	ds, Error = CreateDataSource("SPBFUT", "BRQ8", INTERVAL_M15);
 	while (Error == "" or Error == nil) and ds:Size() == 0 do sleep(1000) message('додж') end
 	if Error ~= "" and Error ~= nil then message("Ошибка подключения к графику: "..Error) return end
 	-- Чтобы получать новые данные без использования функции обратного вызова, а просто получать новые данные в ds и брать их оттуда по необходимости существует функция:
@@ -34,15 +42,16 @@ end;
 
 -- функция остановки скрипта
 function OnStop()
+	message("STOP SCRIPT");
 	isRun = false;
 end;
 
--- 
-function feelingSequence(n)
+-- вычисление среднего значения тел свечек за n-периодов
+function avrBodyCandle(n)
 	size = ds:Size();
 	arrCandle = {};
 	temp = 0;
-	for i=0, n do
+	for i=1, n do
 		temp = temp + math.abs(ds:O(size-i)-ds:C(size-i));
 		--arrCandle[i] = ds:O(size-i)-ds:C(size-i);
 	end;
